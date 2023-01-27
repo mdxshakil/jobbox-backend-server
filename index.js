f21de61035d5adfff73dfe5d5c44f995c72cd285
mdxshakil <mdxshakil1767@gmail.com>
2023-01-27 10:38:03 +0600

    user collection api setup

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.omdlsoo.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+//middlewares
+app.use(express.json())
+app.use(cors());
+
+//mongodb
+async function run() {
+    try {
+        await client.connect()
+        const usersCollection = client.db('JOBBOX').collection('users');
+
+        app.get('/users',async(req,res)=>{
+            const result = await usersCollection.find({}).toArray();
+            res.send(result)
+        })
+        app.post('/user', async (req, res) => {
+            const userData = req.body;
+            const result = await usersCollection.insertOne(userData);
+            res.status(200).send(result);
+        })
+
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send("<h1>Hello from JOB BOX server</h1>")
+})
+app.listen(port, () => {
+    console.log(`Listening to port ${port}`);
+})
